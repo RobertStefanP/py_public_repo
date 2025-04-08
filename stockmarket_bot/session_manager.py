ac47df243e6aa5bdd9f9fde586ec4bfaefe665ed
RobertStefanP <155854085+RobertStefanP@users.noreply.github.com>
2025-04-08 15:41:59 +0200

    08.04.2025

--- stockmarket_bot/session_manager.py
+++ stockmarket_bot/session_manager.py
@@ -27,40 +27,37 @@ class SessionManager:
                     order = trade.order                                
                     contract = trade.contract
                     order_status = trade.orderStatus 
-                    try:                        
-                        if order_status and order_status.status in ('Submitted', 'PreSubmitted'):
-                            if (contract.symbol == self.contract.symbol and
-                                contract.lastTradeDateOrContractMonth == self.contract.lastTradeDateOrContractMonth and
-                                contract.exchange == self.contract.exchange):                             
-                                relevant_orders.append(order) 
-                                
-                                if order.parentId != 0:                                    
-                                    if order.parentId not in brackets:  
-                                        brackets[order.parentId] = {
-                                            "SellLimit": None, 
-                                            "SellStop": None, 
-                                            "BuyLimit": None, 
-                                            "BuyStop": None, 
-                                            "StopLimitOrder": None,
-                                        }                                                                   
-                                    if order.action == "BUY":  
-                                        if order.orderType == "LMT":
-                                            brackets[order.parentId]["BuyLimit"] = order.lmtPrice
-                                        elif order.orderType == "STP":
-                                            brackets[order.parentId]["BuyStop"] = order.auxPrice
-                                        elif order.orderType == "STP LMT":  
-                                            brackets[order.parentId]["StopLimitOrder"] = order.auxPrice
-                                    
-                                    elif order.action == "SELL":  
-                                        if order.orderType == "LMT":
-                                            brackets[order.parentId]["SellLimit"] = order.lmtPrice
-                                        elif order.orderType == "STP":
-                                            brackets[order.parentId]["SellStop"] = order.auxPrice
-                                        elif order.orderType == "STP LMT":  
-                                            brackets[order.parentId]["StopLimitOrder"] = order.auxPrice 
-                    except Exception as e: 
-                        print_error(str(e))    
                                             
+                    if order_status and order_status.status in ('Submitted', 'PreSubmitted'):
+                        if (contract.symbol == self.contract.symbol and
+                            contract.lastTradeDateOrContractMonth == self.contract.lastTradeDateOrContractMonth and
+                            contract.exchange == self.contract.exchange):                             
+                            relevant_orders.append(order) 
+                            
+                            if order.parentId != 0:                                    
+                                if order.parentId not in brackets:  
+                                    brackets[order.parentId] = {
+                                        "SellLimit": None, 
+                                        "SellStop": None, 
+                                        "BuyLimit": None, 
+                                        "BuyStop": None, 
+                                        "StopLimitOrder": None,
+                                    }                                                                   
+                                if order.action == "BUY":  
+                                    if order.orderType == "LMT":
+                                        brackets[order.parentId]["BuyLimit"] = order.lmtPrice
+                                    elif order.orderType == "STP":
+                                        brackets[order.parentId]["BuyStop"] = order.auxPrice
+                                    elif order.orderType == "STP LMT":  
+                                        brackets[order.parentId]["StopLimitOrder"] = order.auxPrice
+                                
+                                elif order.action == "SELL":  
+                                    if order.orderType == "LMT":
+                                        brackets[order.parentId]["SellLimit"] = order.lmtPrice
+                                    elif order.orderType == "STP":
+                                        brackets[order.parentId]["SellStop"] = order.auxPrice
+                                    elif order.orderType == "STP LMT":  
+                                        brackets[order.parentId]["StopLimitOrder"] = order.auxPrice                                                 
                 self.open_orders = relevant_orders                  
                 print_orders(relevant_orders, brackets) 
 
@@ -77,34 +74,37 @@ class SessionManager:
             return [], {}, f"Error in check_open_orders: {str(e)}"
 
     def check_positions(self):
-        self.open_positions.clear()
-        positions = self.ib.positions()
-        position_details = []
+        try:
+            self.open_positions.clear()
+            positions = self.ib.positions()
+            position_details = []        
+            if positions:            
+                self.open_positions = [
+                    pos for pos in positions
+                    if pos.contract.symbol == self.contract.symbol and
+                    pos.contract.lastTradeDateOrContractMonth == self.contract.lastTradeDateOrContractMonth
+                ]
+                position_details = []
 
-        if positions:            
-            self.open_positions = [
-                pos for pos in positions
-                if pos.contract.symbol == self.contract.symbol and
-                pos.contract.lastTradeDateOrContractMonth == self.contract.lastTradeDateOrContractMonth
-            ]
-            position_details = []
+                for pos in self.open_positions:
+                    avg_cost_adjusted = pos.avgCost / float(pos.contract.multiplier)
+                    rounded_avg_cost = round(avg_cost_adjusted * 4) / 4
+                    formatted_avg_cost = f"{rounded_avg_cost:.2f}"
 
-            for pos in self.open_positions:
-                avg_cost_adjusted = pos.avgCost / float(pos.contract.multiplier)
-                rounded_avg_cost = round(avg_cost_adjusted * 4) / 4
-                formatted_avg_cost = f"{rounded_avg_cost:.2f}"
+                    position_details.append({
+                        "symbol": pos.contract.symbol,
+                        "position": pos.position,
+                        "avg_cost": formatted_avg_cost
+                    })  
 
-                position_details.append({
-                    "symbol": pos.contract.symbol,
-                    "position": pos.position,
-                    "avg_cost": formatted_avg_cost
-                })  
-
+                    print_positions(position_details)
+                self.monitoring_orders_mode = True 
+                
+                return position_details                  
+            else:
+                self.monitoring_orders_mode = False
                 print_positions(position_details)
-            self.monitoring_orders_mode = True 
-               
-            return position_details                  
-        else:
-            self.monitoring_orders_mode = False
-            print_positions(position_details)
+                    
+        except Exception as e: 
+            print_error(str(e))
                         
