e0b6d661db19ee430cd91d240929a3bb98f5a397
RobertStefanP <155854085+RobertStefanP@users.noreply.github.com>
2025-05-17 16:16:39 +0200

    17.05.2025

---
+++ sm_bot/telegram_msg.py
@@ -0,0 +1,10 @@
+import requests
+
+
+def telegr_msg(message):            
+    # Send to Telegram
+    BOT_TOKEN = '??'
+    CHAT_ID = '??'
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+    payload = {'chat_id': CHAT_ID, 'text': message}
+    requests.post(url, data=payload)
